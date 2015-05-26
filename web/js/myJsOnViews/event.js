659c4bbc301c6b027d51fda9433556f7df02a5c8
bezdatiuzer <bezdatiuzer@192.168.8.9>
2015-05-26 11:02:00 +0500

    Merge origin/master
    Conflicts:
    	web/WEB-INF/views/event.jsp

--- web/js/myJsOnViews/event.js
+++ web/js/myJsOnViews/event.js
@@ -14,7 +14,7 @@ $(function(){
     $('.changebleParam').dblclick(function(){
         var changebleElem = $(this);
         var paramType = $(this).attr('id');
-        var cientId = $('#moduleBufer').data('clientId');
+        var cientId = $('#moduleBufer').attr('[data-clientId]');
         var value = $(this).text();
         var input = "<input type=text id='" + cientId + "' class='inp' name='" + paramType + "' value='" + value + "'/>";
         
@@ -22,7 +22,7 @@ $(function(){
         $(document).on('click', function(event) {
             var target = $(event.target);
             var newVal = $('.inp').val();
-            if (target !== input) {
+            if (target.attr('name') !== input.attr('name')) {
                 $.ajax({
                     url:"CallCentr/Client/updateClientFromUser?clientId="+cientId+"&param="+paramType+"&newVal="+newVal,
                     dataType : "json",
